6b01e5c77be3f1c0db1d23edfb100f10e601247d
ArjunMahesh97 <arjunmahesh97@gmail.com>
2019-09-12 00:37:44 +0530

    movement

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    Rigidbody rb;
+
+    [SerializeField] float forwardVelocity = 10f;
+    [SerializeField] float sideVelocity = 10f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Movement();
+    }
+
+    private void Movement()
+    {
+        if (Input.GetKey("w"))
+        {
+            float forVel = Input.GetAxis("Vertical");
+            float sideVel = Input.GetAxis("Horizontal");
+            Vector3 vel = new Vector3(sideVelocity * sideVel, 0, forwardVelocity * forVel);
+            rb.velocity = vel;
+        }
+        else
+        {
+            rb.velocity = rb.velocity;
+            rb.rotation = gameObject.transform.rotation;
+        }
+    }
+}
